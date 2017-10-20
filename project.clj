1ba5f033223b2b806be7738a042382b3b613eace
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-10-20 14:44:32 +0200

    lein-release plugin: bumped version from 0.5.5 to 0.5.6-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet "0.5.5"
+(defproject dda/dda-pallet "0.5.6-SNAPSHOT"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :pallet {:source-paths ["src"]}
