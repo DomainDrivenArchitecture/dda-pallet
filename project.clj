e52ee6f39007a42ff223cd0c4106c872386cd1e1
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-12-06 09:05:18 +0100

    lein-release plugin: preparing 0.6.3 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet "0.6.3-SNAPSHOT"
+(defproject dda/dda-pallet "0.6.3"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :pallet {:source-paths ["src"]}
