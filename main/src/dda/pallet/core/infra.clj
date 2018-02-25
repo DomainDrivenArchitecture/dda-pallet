b6a2d88c74e20cadf0425b60b2cc28838694864a
jem <michael.jerger@meissa-gmbh.de>
2018-02-25 22:48:11 +0100

    adjust toString

--- main/src/dda/pallet/core/infra.clj
+++ main/src/dda/pallet/core/infra.clj
@@ -49,8 +49,7 @@
   [facility :- s/Keyword
    infra-schema :- {s/Any s/Any}]
   Object
-  (toString [_] (str "DdaCrateInfra[facility=" (:facility _)
-                     " ver=" (:version _) "]")))
+  (toString [_] (str "DdaCrateInfra[facility=" (:facility _) "]")))
 
 (s/defn dispatch-by-crate-facility :- s/Keyword
   "Dispatcher for phase multimethods by facility. Also does a
