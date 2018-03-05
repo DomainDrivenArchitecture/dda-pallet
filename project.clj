b3eea21d1dfe532a11936c181fac17f58483044f
jem <michael.jerger@meissa-gmbh.de>
2018-03-05 19:13:53 +0100

    Version 2.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet "2.0.0-SNAPSHOT"
+(defproject dda/dda-pallet "2.0.0"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :pallet {:source-paths ["src"]}
