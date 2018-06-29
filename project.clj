847afa7d642cebcd414c52689c600715900b94df
jem <michael.jerger@meissa-gmbh.de>
2018-06-29 18:26:11 +0200

    Version 2.2.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-pallet "2.2.1"
+(defproject dda/dda-pallet "2.2.2-SNAPSHOT"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
