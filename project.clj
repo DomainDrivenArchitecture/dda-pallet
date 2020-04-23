b7b5f39ac2e45fcde37d1f839b16692dee7b5d39
jem <michael.jerger@meissa-gmbh.de>
2020-04-23 18:49:09 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject dda/dda-pallet "3.0.1-SNAPSHOT"
+(defproject dda/dda-pallet "3.1.0-SNAPSHOT"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [commons-codec "1.12"]
-                 [prismatic/schema "1.1.10"]
-                 [selmer "1.12.12"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [commons-codec "1.14"]
+                 [prismatic/schema "1.1.12"]
+                 [selmer "1.12.23"]
                  [dda/pallet "0.9.1"]
                  [dda/dda-config-commons "1.5.0"]
-                 [dda/dda-pallet-commons "1.5.0"]]
+                 [dda/dda-pallet-commons "1.6.0"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -22,10 +22,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha4"]
+                   [[org.clojure/test.check "1.0.0"]
                     [dda/pallet "0.9.1" :classifier "tests"]
-                    [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
+                    [ch.qos.logback/logback-classic "1.3.0-alpha5"]
+                    [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
                    :plugins
                    [[lein-sub "0.3.0"]]
                    :leiningen/reply
