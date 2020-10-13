c9ddec5ba839f4d2e1a80df10e9c3a70806c63de
jem <michael.jerger@meissa-gmbh.de>
2020-10-13 13:47:56 +0200

    Version 4.0.1

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject dda/dda-pallet "4.0.1-SNAPSHOT"
+(defproject dda/dda-pallet "4.0.1"
   :description "The dda-crate"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [commons-codec "1.14"]
+                 [commons-codec "1.15"]
                  [prismatic/schema "1.1.12"]
-                 [selmer "1.12.27"]
+                 [selmer "1.12.31"]
                  [dda/pallet "0.9.1"]
                  [dda/dda-config-commons "1.5.0"]
-                 [dda/dda-pallet-commons "1.6.4"]]
+                 [dda/dda-pallet-commons "1.6.5"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
@@ -22,7 +22,7 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "1.0.0"]
+                   [[org.clojure/test.check "1.1.0"]
                     [dda/pallet "0.9.1" :classifier "tests"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha5"]
                     [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
